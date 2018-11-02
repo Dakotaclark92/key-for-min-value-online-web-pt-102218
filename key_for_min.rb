2252bf87abb7d56ad1b83699222357200852a974
Dakota Clark <bannaray95@gmail.com>
2018-11-02 04:20:58 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+paper = {"letter" => -50, "legal" => -25, "cardstock" => -100}
+key_for_min_value(paper)
